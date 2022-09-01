fbea29b08a0c9c05c9ccd69064408e3b7e86c789
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2022-09-01 18:44:06 -0500

    Update useAPI.ts

--- hooks/useAPI.ts
+++ hooks/useAPI.ts
@@ -1,19 +1,33 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import useBackbone from "./useBackbone";
 
-export default function useAPI() {
+export default function useAPI(onAdd?) {
     const backbone = useBackbone() 
-    const [API, setAPI]: [{[key: string]: Function}, Dispatch<SetStateAction<{[key: string]: Function}>>] = useState({});
-
+    const [API, setAPI] = useState<{[key: string]: Function}>({});
+    const [stream, setStream] = useState<{data: any[], change: any}>({data: [], change: null});
+    
     useEffect(()=> {
         if (backbone) {
+            // Filter user defined API functions
             for (let key of Object.keys(backbone)) {
-                if (key !== "UI" && key !== "_" && key !== "network" && key !== "meta" && key !== "users") {
+                if (!["backboneReactOnAdd","backboneReactAll","UI", "_", "network", "meta", "users"].includes(key)) {
                     API[key] = backbone[key];
                 }
             }
+            // Check for dependency functions
+            if (API?.backboneReactOnAdd && API?.backboneReactAll) {
+                // Override default listener
+                if (onAdd) API.backboneReactOnAdd(onAdd);
+                // Listen for changes to the apps Data
+                else { 
+                    API.backboneReactOnAdd(async ()=> {
+                        let all = await API.backboneReactAll()
+                        setStream({data: all, change: all[all.length - 1]})
+                    })
+                }
+            } else console.warn("backbone-react is missing dependencies in src/app/api.ts, some feautres will be disabled. Learn more at https://github.com/backbonedao/backbone-react/blob/main/README.md#useapi")
         } 
     }, [backbone])
     
-    return API
+    return { API, stream }
 }
